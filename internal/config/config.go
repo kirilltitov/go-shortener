339c7fe5f371ac8f0f85f0b0ff533fd929dcbd87
Kirill Titov <kirill@kirilltitov.com>
2025-02-06 16:27:39 +0300

    JSON-конфиг

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,6 +2,9 @@ package config
 
 import (
 	"os"
+
+	"github.com/kirilltitov/go-shortener/internal/config/json"
+	"github.com/kirilltitov/go-shortener/internal/logger"
 )
 
 // Config является объектом для хранения конфигурации сервиса.
@@ -35,16 +38,32 @@ func New() Config {
 
 // NewWithoutParsing создает и возвращает экземпляр конфигурации сервиса без парсинга флагов.
 func NewWithoutParsing() Config {
+	var jsonConfig json.Config
+	if jsonFilePath := getJSONFilePath(); jsonFilePath != "" {
+		data, err := json.Read(jsonFilePath)
+		if err != nil {
+			logger.Log.Infof("Could not load JSON config file: %s, ignoring", err)
+		} else {
+			c, err2 := json.Load(data)
+			if err2 != nil {
+				logger.Log.Infof("Could not load JSON config file: %s, ignoring", err2)
+			} else {
+				jsonConfig = *c
+				logger.Log.Infof("Loaded config file %s with config %+v", jsonFilePath, jsonConfig)
+			}
+		}
+	}
+
 	return Config{
-		ServerAddress:   getServerAddress(),
-		BaseURL:         getBaseURL(),
-		FileStoragePath: getFileStoragePath(),
-		DatabaseDSN:     getDatabaseDSN(),
-		EnableHTTPS:     getEnableHTTPS(),
+		ServerAddress:   getServerAddress(jsonConfig),
+		BaseURL:         getBaseURL(jsonConfig),
+		FileStoragePath: getFileStoragePath(jsonConfig),
+		DatabaseDSN:     getDatabaseDSN(jsonConfig),
+		EnableHTTPS:     getEnableHTTPS(jsonConfig),
 	}
 }
 
-func getServerAddress() string {
+func getServerAddress(jsonConfig json.Config) string {
 	var result = flagBind
 
 	envServerAddress := os.Getenv("SERVER_ADDRESS")
@@ -52,10 +71,14 @@ func getServerAddress() string {
 		result = envServerAddress
 	}
 
+	if result == "" && jsonConfig.ServerAddress != "" {
+		result = jsonConfig.ServerAddress
+	}
+
 	return result
 }
 
-func getBaseURL() string {
+func getBaseURL(jsonConfig json.Config) string {
 	var result = flagBaseURL
 
 	envBaseURL := os.Getenv("BASE_URL")
@@ -63,10 +86,14 @@ func getBaseURL() string {
 		result = envBaseURL
 	}
 
+	if result == "" && jsonConfig.BaseURL != "" {
+		result = jsonConfig.BaseURL
+	}
+
 	return result
 }
 
-func getFileStoragePath() string {
+func getFileStoragePath(jsonConfig json.Config) string {
 	var result = flagFileStoragePath
 
 	envFileStoragePath := os.Getenv("FILE_STORAGE_PATH")
@@ -74,10 +101,14 @@ func getFileStoragePath() string {
 		result = envFileStoragePath
 	}
 
+	if result == "" && jsonConfig.FileStoragePath != "" {
+		result = jsonConfig.FileStoragePath
+	}
+
 	return result
 }
 
-func getDatabaseDSN() string {
+func getDatabaseDSN(jsonConfig json.Config) string {
 	var result = flagDatabaseDSN
 
 	envDatabaseDSN := os.Getenv("DATABASE_DSN")
@@ -85,15 +116,34 @@ func getDatabaseDSN() string {
 		result = envDatabaseDSN
 	}
 
+	if result == "" && jsonConfig.DatabaseDSN != "" {
+		result = jsonConfig.DatabaseDSN
+	}
+
 	return result
 }
 
-func getEnableHTTPS() string {
+func getEnableHTTPS(jsonConfig json.Config) string {
 	var result = flagEnableHTTPS
 
 	envEnableHTTPS := os.Getenv("ENABLE_HTTPS")
 	if envEnableHTTPS != "" {
 		result = envEnableHTTPS
+	}
+
+	if result == "" && jsonConfig.EnableHTTPS == true {
+		result = "true"
+	}
+
+	return result
+}
+
+func getJSONFilePath() string {
+	var result = flagJSONConfig
+
+	JSONConfigPath := os.Getenv("CONFIG")
+	if JSONConfigPath != "" {
+		result = JSONConfigPath
 	}
 
 	return result
--- internal/config/flags.go
+++ internal/config/flags.go
@@ -12,6 +12,7 @@ var flagBaseURL = fmt.Sprintf("http://localhost:%d", defaultPort)
 var flagFileStoragePath = "/tmp/short-url-db.json"
 var flagDatabaseDSN = ""
 var flagEnableHTTPS = ""
+var flagJSONConfig = ""
 
 func parseFlags() {
 	flag.StringVar(&flagBind, "a", flagBind, "Host and port to bind")
@@ -19,6 +20,7 @@ func parseFlags() {
 	flag.StringVar(&flagFileStoragePath, "f", flagFileStoragePath, "File storage path")
 	flag.StringVar(&flagDatabaseDSN, "d", flagDatabaseDSN, "Database DSN")
 	flag.StringVar(&flagEnableHTTPS, "s", flagEnableHTTPS, "Enable HTTPS")
+	flag.StringVar(&flagJSONConfig, "c", flagJSONConfig, "Path to JSON config file")
 
 	flag.Parse()
 }
