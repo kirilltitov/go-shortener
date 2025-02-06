b873ebcc0747b4d11a3095444ea190c6326a53e1
Kirill Titov <kirill@kirilltitov.com>
2025-02-07 00:30:48 +0300

    Документация

--- internal/shortener/stats.go
+++ internal/shortener/stats.go
@@ -6,6 +6,7 @@ import (
 	"github.com/kirilltitov/go-shortener/internal/storage"
 )
 
+// GetStats возвращает статистику использования сервиса, см. структуру [storage.Stats].
 func (s *Shortener) GetStats(ctx context.Context) (*storage.Stats, error) {
 	return s.container.Storage.GetStats(ctx)
 }
